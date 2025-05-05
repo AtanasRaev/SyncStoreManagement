281510f0024314cae7fee4c7abcd16f9fb191ff6
AtanasRaev <atanaspraev@gmail.com>
2025-05-06 01:12:58 +0300

    Update Purchase model to replace isSynced with appId
    Replaced the isSynced field with a unique appId column for better identification of purchase records. This change simplifies synchronization logic and ensures unique tracking within the database.

--- src/main/java/com/syncstoremanagement/database/model/Purchase.java
+++ src/main/java/com/syncstoremanagement/database/model/Purchase.java
@@ -29,17 +29,6 @@ public class Purchase {
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "is_synced", nullable = false)
-    private boolean isSynced;
-
-    public Purchase(Product product, BigDecimal quantity, BigDecimal price, LocalDateTime createdAt, boolean isSynced) {
-        this.product = product;
-        this.quantity = quantity;
-        this.price = price;
-        this.createdAt = createdAt;
-        this.isSynced = isSynced;
-    }
-
-    public Purchase() {
-    }
+    @Column(name = "app_id", unique = true)
+    private Long appId;
 }

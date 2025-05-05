b2c75686472d3ad872c1d8f632c76849fb92670f
AtanasRaev <atanaspraev@gmail.com>
2025-05-06 01:15:20 +0300

    Add SyncLogReport entity for database synchronization logs
    Introduced the SyncLogReport model, mapping it to the `sync_log_reports` table in the database. This entity captures details such as new and updated row counts and their timestamps, aiding in synchronization reporting and tracking.

---
+++ src/main/java/com/syncstoremanagement/database/model/SyncLogReport.java
@@ -0,0 +1,35 @@
+package com.syncstoremanagement.database.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "sync_log_reports")
+public class SyncLogReport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "new_created_rows")
+    private int newCreatedRows;
+
+    @Column(name = "updated_rows")
+    private int updatedRows;
+
+    public SyncLogReport(int newCreatedRows, int updatedRows) {
+        this.newCreatedRows = newCreatedRows;
+        this.updatedRows = updatedRows;
+    }
+
+    public SyncLogReport() {
+    }
+}
